cdc754f73a339f5ef982d107327bee6c726f172b
ruan65 <ruan65@gmail.com>
2017-07-12 18:18:29 +0300

    Search interactor

---
+++ app/src/main/java/org/premiumapp/celeba/buisness_logic/interactor/search/SearchInteractor.java
@@ -0,0 +1,34 @@
+package org.premiumapp.celeba.buisness_logic.interactor.search;
+
+import org.premiumapp.celeba.buisness_logic.search_engine.SearchEngine;
+
+import io.reactivex.Observable;
+
+/**
+ * Created by a on 12.07.17.
+ */
+
+public class SearchInteractor {
+
+    private final SearchEngine searchEngine;
+
+    public SearchInteractor(SearchEngine searchEngine) {
+        this.searchEngine = searchEngine;
+    }
+
+    public Observable<SearchViewState> search(String query) {
+
+        if (query.isEmpty()) {
+            return Observable.just(new SearchViewState.SearchNotStartedYet());
+        }
+
+        return searchEngine.searchFor(query)
+                .map(people -> {
+                    if (people.isEmpty()) {
+                        return new SearchViewState.EmptyResult(query);
+                    } else return new SearchViewState.SearchResult(query, people);
+                })
+                .startWith(new SearchViewState.Loading())
+                .onErrorReturn(er -> new SearchViewState.Error(query, er));
+    }
+}
--- app/src/main/java/org/premiumapp/celeba/buisness_logic/search_engine/SearchEngine.java
+++ app/src/main/java/org/premiumapp/celeba/buisness_logic/search_engine/SearchEngine.java
@@ -1,10 +1,37 @@
 package org.premiumapp.celeba.buisness_logic.search_engine;
 
+import android.support.annotation.NonNull;
+
+import org.premiumapp.celeba.buisness_logic.model.TmdbPerson;
+import org.premiumapp.celeba.buisness_logic.networking.TmdbApiDecorator;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+
 /**
  * Created by a on 12.07.17.
  */
 
 public class SearchEngine {
 
+    private final TmdbApiDecorator backend;
 
+    public SearchEngine(TmdbApiDecorator backend) {
+        this.backend = backend;
+    }
+
+    public Observable<List<TmdbPerson>> searchFor(@NonNull String query) {
+
+        if (query.length() < 3) {
+            return Observable.error(new IllegalArgumentException("Search query should be > 3 chars"));
+        }
+
+        return backend.getPersonSearchResult(query)
+                .delay(200, TimeUnit.MICROSECONDS)
+                .flatMap(result -> Observable.fromIterable(result))
+                .toList()
+                .toObservable();
+    }
 }
